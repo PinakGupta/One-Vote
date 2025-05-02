b90385de23af323d52457a1f674411587933a1c1
Sagar <sagar20032610@gmail.com>
2025-05-02 18:02:36 +0530

    cors error removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-     // server: {
-     //      proxy: {
-     //           '/': {
-     //                target: 'http://localhost:3000/',
-     //                changeOrigin:true,
-     //           }
-     //      }
-     // },
      plugins: [react()],
      build: {
           outDir: 'dist',
+     },
+     server: {
+          // disable Vite’s default liberal CORS in dev
+          // cors: false,
+          // or to allow specific origins in dev:
+          cors: {
+            origin: ['http://localhost:5173', 'https://one-vote-taupe.vercel.app']
+          }
      }
 })
