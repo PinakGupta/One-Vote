7fd5e0557ddd942f7d88c496762a955a5ab45d69
Sagar <sagar20032610@gmail.com>
2025-05-02 17:31:42 +0530

    frontend deployment

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
 
 
-export const server = 'http://localhost:8000/api/v1'
-export const serverWithId = 'http://localhost:8000'
+export const server = 'https://one-vote-rtjj.onrender.com/api/v1'
+export const serverWithId = 'https://one-vote-rtjj.onrender.com'
